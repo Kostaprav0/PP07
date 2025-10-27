b6e941d819cf0f4ac6bb8fcf54f8a19a6bd18fe9
Иван <148747156+Kostaprav0@users.noreply.github.com>
2025-10-27 21:17:41 +0300

    Add files via upload

---
+++ Соадминистрирование_баз_данных_и_серверов_2_практика/Src/Бетон2/Бетон2/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Бетон2
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("name=DefaultConnection")
+        {
+        }
+
+        public ApplicationDbContext(string connectionString) : base(connectionString)
+        {
+        }
+
+        public string GetDatabaseInfo()
+        {
+            try
+            {
+                return Database.Connection.Database;
+            }
+            catch
+            {
+                return "Не подключено";
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
